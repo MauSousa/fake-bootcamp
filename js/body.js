f4391b5ed98d80f65b557f650c955ab873e1e50d
Augusto Sousa <lagrange.tesla@gmail.com>
2021-10-03 20:57:38 -0500

    Changed order of code

--- js/body.js
+++ js/body.js
@@ -14,20 +14,7 @@ export const init = () => {
   const BoxesSection = document.createElement('section')
   const LearnSection = document.createElement('section')
   const LearnReactSection = document.createElement('section')
-
-  NavBar.setAttribute('class', 'navbar navbar-expand-lg bg-dark navbar-dark py-3')
-
-  ShowCaseSection.setAttribute('class', 'bg-dark text-light p-5 p-lg-3 pt-lg-5 text-center text-sm-start')
-
-  SectionNewsLetter.setAttribute('class', 'bg-primary text-light p-5')
-
-  BoxesSection.setAttribute('class', 'p-3 g-4')
   
-  LearnSection.classList.add('p-5')
-  LearnSection.setAttribute('id', 'learn')
-  
-  LearnReactSection.setAttribute('class', 'p-5 bg-dark text-light')
-
   const navbar = createNavBar()
   const sectionWebDev = showcase()
   const sectionNewsLetter = newsletter()
@@ -35,6 +22,18 @@ export const init = () => {
   const learn = learnSection()
   const learnreact = learnReactSection()
 
+  NavBar.setAttribute('class', 'navbar navbar-expand-lg bg-dark navbar-dark py-3')
+
+  ShowCaseSection.setAttribute('class', 'bg-dark text-light p-5 p-lg-3 pt-lg-5 text-center text-sm-start')
+
+  SectionNewsLetter.setAttribute('class', 'bg-primary text-light p-5')
+
+  BoxesSection.setAttribute('class', 'p-3')
+  
+  LearnSection.classList.add('p-5')
+  LearnSection.setAttribute('id', 'learn')
+  
+  LearnReactSection.setAttribute('class', 'p-5 bg-dark text-light')
 
   NavBar.innerHTML = navbar
   ShowCaseSection.innerHTML = sectionWebDev
